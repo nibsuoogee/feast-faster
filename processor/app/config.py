3a1a6f5a7a883c50d52df82f12e6498c567bdd53
elidare <elidare.jjj@gmail.com>
2025-11-07 21:07:08 +0200

    Add origins and methods to CORS

--- processor/app/config.py
+++ processor/app/config.py
@@ -15,9 +15,11 @@ class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv("DATABASE_URL")
     OPEN_ROUTE_SERVICE_API_KEY: str = os.getenv("OPEN_ROUTE_SERVICE_API_KEY")
     # CORS settings
-    CORS_ORIGINS: list[str] = ["*"]
+    CORS_ORIGINS: list[str] = [
+        "http://localhost:5173", "https://app.localhost", "http://localhost:3000", "https://backend.localhost"
+    ]
     CORS_HEADERS: list[str] = ["*"]
-    CORS_METHODS: list[str] = ["*"]
+    CORS_METHODS: list[str] = ["POST", "GET", "OPTIONS", "PATCH", "PUT"]
 
     class Config:
         env_file = ".env"
