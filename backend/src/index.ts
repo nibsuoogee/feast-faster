a6e90913b7ecdb3540b2d0b9937b830325fdfb99
Fatima <fatimaakhtar9397@gmail.com>
2025-11-16 20:14:06 +0200

    Update index.ts and docker-compose.yml

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,15 @@ import cors from "@elysiajs/cors";
 import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import { settingsRouter } from "./routes/settingsRouter";
-import { menuItemsRouter } from "./routes/menuItemsRouter"; //  Added this line
+import { menuItemsRouter } from "./routes/menuItemsRouter"; 
 
 const app = new Elysia()
   .use(swagger())
   .use(cors())
   .get("/", () => "Hello Elysia")
   .use(settingsRouter)
-  .use(menuItemsRouter) // Added this line
-  .listen({ port: 3000, hostname: "0.0.0.0" }); // Updated hostname to "
+  .use(menuItemsRouter) 
+  .listen({ port: 3000, hostname: "0.0.0.0" }); 
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
