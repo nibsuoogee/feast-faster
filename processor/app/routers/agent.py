dd8075d1f23e44e0666045791f7d3ae5ad33a32e
elidare <elidare.jjj@gmail.com>
2025-11-23 23:17:09 +0200

    Add POST /calculate-eta

--- processor/app/routers/agent.py
+++ processor/app/routers/agent.py
@@ -1,8 +1,8 @@
 from app.dependencies.database import get_session
 from app.services.open_route import get_location_range, get_driving_etas, RoutingServiceError
 from app.services.charging_estimation import get_estimate_charging_time
-from app.services.database import get_stations_from_db
-from app.models import StationRequest
+from app.services.database import get_stations_from_db, get_destination
+from app.models import StationRequest, ETACalculationRequest
 from app.config import logger
 from sqlmodel import Session
 from fastapi import APIRouter, Depends, HTTPException
@@ -44,3 +44,20 @@ def get_filtered_stations(
     stations_sorted = sorted(stations_with_charging_time, key=lambda x: x["distance_km"])
 
     return stations_sorted
+
+
+@router.post("/calculate-eta")
+def calculate_eta(
+    body: ETACalculationRequest,
+    session: Session = Depends(get_session)
+):
+    # Get destination station location
+    destination_station = get_destination(session, body.reservation_id)
+
+    if not destination_station:
+        raise HTTPException(status_code=404, detail=f"Station not found for reservation with reservation_id {body.reservation_id}")
+
+    # Get ETA and distance
+    station_with_eta = get_driving_etas(body.current_location, [destination_station])
+
+    return station_with_eta
